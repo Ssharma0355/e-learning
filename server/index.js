4a951e577e10dfece95868ce050a5e8a53e87ef4
sachin_sde <ssachin.s@myclnq.co.in>
2025-03-03 21:24:34 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./database/db.js";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 2020;
+const port = process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.send("Server is working fine")
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+  connectDb();
 });
