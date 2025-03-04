e414f6a3b62ae683fc9699652e1649666e6c445c
sachin_sde <ssachin.s@myclnq.co.in>
2025-03-04 17:04:52 +0530

    user-model-api

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+
+const schema = new mongoose.Schema({
+    name:{
+        type:String,
+        require: true
+    },
+
+     email:{
+        type:String,
+        require: true,
+        unique:true,
+    },
+     password:{
+        type:String,
+        require: true
+    },
+    role:{
+        type:String,
+        require:"user"
+    },
+    subscription:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+        }
+    ]
+},
+{
+    timestamps:true,
+}
+)
+
+export const User = mongoose.mondel("User",schema)
